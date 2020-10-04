cff07eb18a1bdc4577d2bbd290fdd1dd443e526e
Zam Hassan <zameerulhassan@gmail.com>
2020-10-03 21:15:20 -0600

    sends json data

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello there"));
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to Contact keeper API..." })
+);
 
 const PORT = process.env.PORT || 5000;
 
