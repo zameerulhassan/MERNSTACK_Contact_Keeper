b46a9e891349b011c8c9845d90b462c23236691c
Zam Hassan <zameerulhassan@gmail.com>
2020-10-03 23:01:29 -0600

    adds MongoDb connection

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
 
+//connect DB
+connectDB();
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to Contact keeper API..." })
 );
