1b7361b78357dc381eb777da4b0ef1b1ac8d14f8
Zam Hassan <zameerulhassan@gmail.com>
2020-10-04 16:02:44 -0600

    adds: 1- models>UserSchema for database 2-express-validator to valid input from api create-request 3-middleware in server.js to parse body

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/Users"); //bringing in user-schema.
+
+//to check and validate
+const { body, validationResult } = require("express-validator");
 
 //@route    POST   api/users
 //@desc     Register a user
 //access    Public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    body("name", "Please enter name").not().isEmpty(),
+    body("password", "Please enter password that is 6 character long").isLength({ min: 6 }),
+    body('email','Please enter a valid email').isEmail()
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('success');
+    //body parser is required as middleware in server js.
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const { init } = require("./models/Users");
 const app = express();
+('express-validator');
+
+
+//parsing body: middleware
+//to accept body-data
+app.use(express.json({extended:false}))
 
 //connect DB
 connectDB();
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to Contact keeper API..." })
 );
+
+
+
 
 //Define Routes
 app.use("/api/contacts", require("./routes/contacts"));
