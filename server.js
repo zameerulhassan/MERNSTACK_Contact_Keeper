9dab9173452f65205fc5384d473d4b6b78087d5b
Zam Hassan <zameerulhassan@gmail.com>
2020-10-03 21:58:54 -0600

    adds api route i.e. 1-api/users 2-api/auth 3-api/contacts

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) =>
   res.json({ msg: "Welcome to Contact keeper API..." })
 );
 
+//Define Routes
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/users", require("./routes/users"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
