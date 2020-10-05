b87ab90d404ad0d45724128a8e09231b7ebc40a4
Zam Hassan <zameerulhassan@gmail.com>
2020-10-05 17:49:44 -0600

    adds: contact POST route to create/add new contact

--- models/Contacts.js
+++ models/Contacts.js
@@ -19,10 +19,6 @@ const ContactSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  password: {
-    type: String,
-    required: true,
-  },
   date: {
     type: Date,
     default: Date.now,
--- routes/contacts.js
+++ routes/contacts.js
@@ -23,9 +23,31 @@ router.get("/", auth, async (req, res) => {
 //@route    POST   api/contacts
 //@desc     Add new contact
 //access    Private
-router.post("/", (req, res) => {
-  res.send("Add contact");
-});
+router.post(
+  "/",
+  [auth, [body("name", "Name is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, phone, type } = req.body;
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 //@route    PUT   api/contacts/:id
 //@desc     Update contact
