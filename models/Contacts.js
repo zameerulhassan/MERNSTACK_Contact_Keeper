c5f1a78192078aedb7d2aeaca3b23ea3dafcc7ff
Zam Hassan <zameerulhassan@gmail.com>
2020-10-05 16:21:41 -0600

    adds:  Contact first rout-GET

---
+++ models/Contacts.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const ContactSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: "personal",
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model("contact", ContactSchema);
+//first parameter is name given to object/schema second is created Schema.
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const User = require("../models/Users"); //bringing in user-schema.
+const Contact = require("../models/Contacts"); //bringing in contact-schema.
+const auth = require("../middleware/auth");
 
 //@route    GET   api/contacts
 //@desc     Get all users contacts
 //access    Private
-router.get("/", (req, res) => {
-  res.send("Get all contact");
+router.get("/", auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 //@route    POST   api/contacts
