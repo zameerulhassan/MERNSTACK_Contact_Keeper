2c28f4cb267c487caed29644f221ebff5a5ed79c
Zam Hassan <zameerulhassan@gmail.com>
2020-10-06 16:28:43 -0600

    adds: 1-Home and About Pages 2-Navbar 3-App.css

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p className="my-1">this is fullstack react app to keep contacts</p>
+      <p className="bg-dark p">
+        <strong>Version:1.0</strong>
+      </p>
+    </div>
+  );
+}
+
+export default About;
