c26c4e8d1a126cf623674f08c85044cc80fd81e7
Zam Hassan <zameerulhassan@gmail.com>
2020-10-16 22:36:03 -0600

    adds: 1-Login Component with local state 2-Register Componenet with local state

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import "./App.css";
 import Navbar from "./components/layouts/Navbar";
 import ContactState from "./context/contact/ContactState";
@@ -15,6 +17,8 @@ const App = () => {
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/about" component={About} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
             </Switch>
           </div>
         </Fragment>
--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -5,7 +5,8 @@ const Navbar = ({ title, icon }) => {
   return (
     <div className="navbar bg-primary">
       <h1>
-        <i className={icon} />{` `}
+        <i className={icon} />
+        {` `}
         {title}
       </h1>
       <ul>
@@ -14,6 +15,12 @@ const Navbar = ({ title, icon }) => {
         </li>
         <li>
           <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
         </li>
       </ul>
     </div>
