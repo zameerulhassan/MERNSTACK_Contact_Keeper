9a0e1445d7e1c786f5c09d9667fec7066c837731
Zam Hassan <zameerulhassan@gmail.com>
2020-10-18 20:55:54 -0600

    adds 1-spinner 2-clearContacts 3-updates contacts,navbar components for conditional rendering

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -1,12 +1,16 @@
 import React, { Fragment, useContext } from "react";
 import AuthContext from "../../context/auth/authContext";
+import ContactContext from "../../context/contact/contactContext";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 const Navbar = ({ title, icon }) => {
   const authContext = useContext(AuthContext);
+  const contactContext = useContext(ContactContext);
   const { isAuthenticated, logout, user } = authContext;
+  const { clearContacts } = contactContext;
   const onLogout = () => {
     logout();
+    clearContacts();
   };
   const authLinks = (
     <Fragment>
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -4,6 +4,8 @@ import contactReducer from "./contactReducer";
 import axios from "axios";
 import {
   ADD_CONTACT,
+  GET_CONTACTS,
+  CLEAR_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
   CLEAR_CURRENT,
@@ -13,33 +15,38 @@ import {
   SET_ALERT,
   REMOVE_ALERT,
   CONTACT_ERROR,
+  CLEAR_CONTACTS,
 } from "../types";
 
 const ContactState = (props) => {
   const initialState = {
-    contacts: [],
+    contacts: null,
     current: null,
     filtered: null,
-    error:null
+    error: null,
   };
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Get Contacts
-  // const getContacts = async () => {
-  //   try {
-  //     const res = await axios.get('/api/contacts');
+  const getContacts = async () => {
+    try {
+      const res = await axios.get("/api/contacts");
 
-  //     dispatch({
-  //       type: GET_CONTACTS,
-  //       payload: res.data
-  //     });
-  //   } catch (err) {
-  //     dispatch({
-  //       type: CONTACT_ERROR,
-  //       payload: err.response.msg
-  //     });
-  //   }
-  // };
+      dispatch({
+        type: GET_CONTACTS,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg,
+      });
+    }
+  };
+  //clearContact
+  const clearContacts=()=>{
+    dispatch({type:CLEAR_CONTACTS})
+  }
 
   //ADD CONTACT
   const addContact = async (contact) => {
@@ -88,7 +95,7 @@ const ContactState = (props) => {
         contacts: state.contacts,
         current: state.current,
         filtered: state.filtered,
-        error:state.error,
+        error: state.error,
         addContact,
         deleteContact,
         setCurrent,
@@ -96,6 +103,8 @@ const ContactState = (props) => {
         updateContact,
         filterContacts,
         clearFilter,
+        getContacts,
+        clearContacts
       }}
     >
       {props.children}
