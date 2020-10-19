85e62c0fed80378df62a8fc752af3e2c38b988f6
Zam Hassan <zameerulhassan@gmail.com>
2020-10-18 22:24:04 -0600

    deploys on heroku

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 const { init } = require("./models/Users");
 const app = express();
-('express-validator');
-
+("express-validator");
 
 //parsing body: middleware
 //to accept body-data
-app.use(express.json({extended:false}))
+app.use(express.json({ extended: false }));
 
 //connect DB
 connectDB();
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to Contact keeper API..." })
-);
-
-
-
 
 //Define Routes
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
 
+//Serve static assest in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
